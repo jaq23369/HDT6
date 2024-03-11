41ede329fc29ec273a8b171b7a18793521f1afd8
Hb_W <hugobarillasvi@hotmail.com>
2024-03-11 11:39:24 -0600

    Cambios minimos qué no se por qué no se había guardado

--- src/main/java/uvg/edu/gt/LinkedHashMapFactory.java
+++ src/main/java/uvg/edu/gt/LinkedHashMapFactory.java
@@ -1,9 +1,19 @@
 package uvg.edu.gt;
-
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+/**
+ * Fábrica que crea instancias de LinkedHashMap.
+ */
 public class LinkedHashMapFactory implements MapFactory {
+
+    /**
+     * Crea y devuelve una nueva instancia de LinkedHashMap.
+     *
+     * @param <K> Tipo de la clave
+     * @param <V> Tipo del valor
+     * @return Nueva instancia de LinkedHashMap
+     */
     @Override
     public <K, V> Map<K, V> createMap() {
         return new LinkedHashMap<>();
